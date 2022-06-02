39cd5c4b827de9d81e788ac1a82cd7a3d7000139
Xayw3 <xaywe1@gmail.com>
2022-06-02 16:19:13 +0300

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="services-wrapper">
+    <div v-for="(service, index) in services" :key="index">
+      <div
+        @click="
+          (service.isActive = !service.isActive),
+            service.isActive
+              ? (total = total + service.price)
+              : (total = total - service.price)
+        "
+        class="item"
+        :class="[service.isActive ? activeClass : '']"
+      >
+        <p>{{ service.name }}</p>
+        <span>${{ service.price }}</span>
+      </div>
+    </div>
+    <div class="item item-total">
+      <h3>Total:</h3>
+      <h3>${{ total }}</h3>
+    </div>
   </div>
 </template>
 
+<style lang="scss">
+@import "src/assets/styles/main.scss";
+</style>
+
 <script lang="ts">
+import Service from "@/models/ServicesModel";
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  data: () => ({
+    activeClass: "active",
+    services: [
+      {
+        name: "Web development",
+        price: 300,
+        isActive: false,
+      },
+      {
+        name: "Design",
+        price: 400,
+        isActive: false,
+      },
+      {
+        name: "Integration",
+        price: 250,
+        isActive: false,
+      },
+      {
+        name: "Training",
+        price: 220,
+        isActive: false,
+      },
+    ] as Service[],
+    total: 0,
+  }),
 });
 </script>
